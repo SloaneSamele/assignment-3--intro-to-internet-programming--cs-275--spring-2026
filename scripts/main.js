4ca923dd14da537a4362556b9ff580c22c37486d
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-04-21 15:01:43 -0400

    Added variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
-let body = document.querySelector(`body`);
-let script = document.createElement(`script`);
+let showMenuButton = document.getElementById(`show-menu`);
+let showModalButton = document.getElementById(`show-modal`);
+let modal = document.querySelector(`.modal-panel`);
+let modalPane = document.querySelector(`.modal-content-pane`);
+let menu = document.querySelector(`nav`);
+let menuShowing = false;
+let modalShowing = false;
 
-body.appendChild(script);
